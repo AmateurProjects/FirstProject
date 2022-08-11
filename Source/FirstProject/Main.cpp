3d4c8ee2e280327338e7010c2618f2e6868e2f6b
AmateurProjects <guarded@gmail.com>
2022-08-11 10:49:54 -0400

    added animations and some gamepad inputs

--- Source/FirstProject/Main.cpp
+++ Source/FirstProject/Main.cpp
@@ -43,7 +43,7 @@ AMain::AMain()
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.f, 0.0f); // ...at this rotation rate
-	GetCharacterMovement()->JumpZVelocity = 350.f;
+	GetCharacterMovement()->JumpZVelocity = 450.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
 
