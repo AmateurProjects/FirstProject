bf8954a9b76b8920c18fa0375957a6b68edc36e6
AmateurProjects <guarded@gmail.com>
2022-08-13 17:11:38 -0400

    added VFX assets and initial spawn volumes

--- Source/FirstProject/FloorSwitch.cpp
+++ Source/FirstProject/FloorSwitch.cpp
@@ -4,6 +4,7 @@
 #include "FloorSwitch.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
 
 // Sets default values
 AFloorSwitch::AFloorSwitch()
@@ -27,6 +28,9 @@ AFloorSwitch::AFloorSwitch()
 	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
 	Door->SetupAttachment(GetRootComponent());
 
+	SwitchTime = 2.f;
+
+	bCharacterOnSwitch = false;
 }
 
 // Called when the game starts or when spawned
@@ -52,6 +56,7 @@ void AFloorSwitch::Tick(float DeltaTime)
 void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Overlap Begin"));
+	if (!bCharacterOnSwitch) bCharacterOnSwitch = true;
 	RaiseDoor();
 	LowerFloorSwitch();
 }
@@ -59,8 +64,8 @@ void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AAct
 void AFloorSwitch::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Overlap End"));
-	LowerDoor();
-	RaiseFloorSwitch();
+	if (bCharacterOnSwitch) bCharacterOnSwitch = false;
+	GetWorldTimerManager().SetTimer(SwitchHandle, this, &AFloorSwitch::CloseDoor, SwitchTime);
 }
 
 void AFloorSwitch::UpdateDoorLocation(float Z)
@@ -77,6 +82,15 @@ void AFloorSwitch::UpdateFloorSwitchLocation(float Z)
 	NewLocation.Z += Z;
 	FloorSwitch->SetWorldLocation(NewLocation);
 
+}
+
+void AFloorSwitch::CloseDoor()
+{
+	if (!bCharacterOnSwitch)
+	{
+	LowerDoor();
+	RaiseFloorSwitch();
+	}
 }
 
 
