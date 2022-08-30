b70a850ddcabe3c49eabee1cff965f2cf1fe1460
AmateurProjects <guarded@gmail.com>
2022-08-29 20:26:24 -0400

    added initial enemy files and AnimInstance

---
+++ Source/FirstProject/EmemyAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EmemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_API UEmemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
